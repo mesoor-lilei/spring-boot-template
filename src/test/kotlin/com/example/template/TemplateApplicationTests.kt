8e90e116812ad868a411509e0ab01ad5f5c85abe
Li Lei <this.lilei@gmail.com>
2021-06-02 17:51:38 +0800

    init

---
+++ src/test/kotlin/com/example/template/TemplateApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.example.template
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TemplateApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
